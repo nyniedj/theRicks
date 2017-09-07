309ef98490ddbd3c2a2985bc39a9f1ce286c5641
nyniedj <nyniedj@gmail.com>
2017-09-07 10:36:39 -0400

    Completed calc method for Person1

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,9 +3,9 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
- *  @author Bob
+ *  modified string
+ *
+ *  @author Bob, Nylah Julmice
  *  @version 1.1
  */
 public class Person1 {
@@ -31,19 +31,24 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+	  String firstTwo = input.substring(0, 2);
+	  return input.substring(2) + firstTwo;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
 	}
 
+	public static void main(String[] args) {
+		Person1 p1 = new Person1("Nylah333c");
+		System.out.println(p1);
+	}
 }
