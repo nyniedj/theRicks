1483520e2790518be0ccf65563d27914bedf87c6
Ben Lashley <blashley3@gatech.edu>
2017-09-19 16:03:21 -0400

    Added activity for registering a user

---
+++ m4/app/src/main/java/edu/gatech/cs2340/thericks/controllers/RegisterActivity.java
@@ -0,0 +1,18 @@
+package edu.gatech.cs2340.thericks.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.cs2340.thericks.R;
+
+/**
+ * Created by Ben Lashley on 9/19/2017.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
