764bf2336ff748562dc5ca319d493cb8bd0ec5f7
Ben Lashley <blashley3@gatech.edu>
2017-09-19 21:08:29 -0400

    Moved password/username validation to Login.java

--- m4/app/src/main/java/edu/gatech/cs2340/thericks/models/Login.java
+++ m4/app/src/main/java/edu/gatech/cs2340/thericks/models/Login.java
@@ -1,8 +1,10 @@
 package edu.gatech.cs2340.thericks.models;
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import java.io.Serializable;
+import java.util.regex.Pattern;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -29,6 +31,9 @@ public class Login implements Serializable {
     /** Random salt for hashing **/
     private byte[] salt;
 
+    /** Whether or not the login information is all valid **/
+    private boolean valid = false;
+
     /** Number of iterations for password encryption **/
     private static final int ITERATIONS = 200000;
 
@@ -38,14 +43,32 @@ public class Login implements Serializable {
     /** Size in bytes of encrypted key **/
     private static final int KEY_LENGTH = 32;
 
+    /** See validatePassword() for complete password criteria **/
+    private static final int MIN_PASSWORD_LENGTH = 8;
+    private static final int MAX_PASSWORD_LENGTH = 32;
+
+    private static final int MIN_USERNAME_LENGTH = 6;
+    private static final int MAX_USERNAME_LENGTH = 32;
+
 
     /**
-     * Constructor for login info
+     * Constructor for new login information
      *
      * @param username user's username
      * @param password user's un-hashed password
      */
     public Login(String username, String password) {
+        // Check that username and password are valid.
+        if (!validateUsername(username)) {
+            Log.e("Login", "Invalid username: " + username);
+            return;
+        }
+        else if (!validatePassword(password)) {
+            Log.e("Login", "Invalid password");
+            return;
+        }
+
+        // Good username/password
         this.username = username;
         salt = generateSalt();
 
@@ -57,10 +80,12 @@ public class Login implements Serializable {
             Log.e("Login", "Invalid specifications for secure key.");
         }
 
-        /* Password encryption failed. Invalidate login info. */
         if (securePassword == null) {
-            salt = null;
-            Log.e("Login", "User login info could not be created.");
+            // Password encryption failed
+            Log.e("Login", "User's encrypted password could not be created.");
+        } else {
+            // Password encryption succeeded
+            valid = true;
         }
     }
 
@@ -93,6 +118,74 @@ public class Login implements Serializable {
     }
 
     /**
+     * Checks if a new user's desired username is valid.
+     *
+     * @param username the desired username
+     * @return true if valid, false if not
+     */
+    private boolean validateUsername(String username) {
+        // Check if username is valid
+        if (username == null) {
+            return false;
+        } else if (username.length() < MIN_USERNAME_LENGTH || username.length() > MAX_USERNAME_LENGTH) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks if password meets criteria to be considered valid.
+     * Criteria (must meet all):
+     *  (?=.*[0-9]) at least one digit
+     *  (?=.*[a-z]) at least one lowercase letter
+     *  (?=.*[A-Z]) at least one uppercase letter
+     *  (?=\\S+$)   no whitespace
+     *  .{MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH}     length between min and max (inclusive)
+     *
+     * @param pw the password to validate
+     * @return true if password is valid, false if not
+     */
+    private boolean validatePassword(String pw) {
+        if (pw == null) {
+            return false;
+        }
+        final Pattern pat = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{"+ MIN_PASSWORD_LENGTH + "," + MAX_PASSWORD_LENGTH + "}$");
+        return pw != null && pat.matcher(pw).matches();
+    }
+
+    /**
+     * Resets current password to new password. If new password is invalid, nothing is changed.
+     *
+     * @param newPassword the new password to be set
+     * @return true if reset was successful, false if not
+     */
+    public boolean resetPassword(String newPassword) {
+        if (!validatePassword(newPassword)) {
+            return false;
+        }
+        salt = generateSalt();
+        try {
+            securePassword = createEncryptedPassword(newPassword, salt, ITERATIONS, KEY_LENGTH);
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("Login", "Algorithm requested for encryption does not exist.");
+            return false;
+        } catch (InvalidKeySpecException e) {
+            Log.e("Login", "Invalid specifications for secure key.");
+            return false;
+        }
+
+        if (securePassword == null) {
+            // Password encryption failed
+            Log.e("Login", "User's encrypted password could not be created.");
+            return false;
+        } else {
+            // Password encryption succeeded
+            valid = true;
+            return true;
+        }
+    }
+
+    /**
      *
      * @return Username for login
      */
@@ -121,11 +214,22 @@ public class Login implements Serializable {
      * @return Secure password as a readable hexadecimal string
      */
     private String getPasswordString() {
+        if (securePassword == null) {
+            return "INVALID";
+        }
         final StringBuilder builder = new StringBuilder();
         for (byte b : securePassword) {
             builder.append(String.format("%02x", b));
         }
         return builder.toString();
+    }
+
+    /**
+     *
+     * @return true if login data is valid, false if not
+     */
+    public boolean isValid() {
+        return valid;
     }
 
     /**
--- m4/app/src/main/java/edu/gatech/cs2340/thericks/models/UserTable.java
+++ m4/app/src/main/java/edu/gatech/cs2340/thericks/models/UserTable.java
@@ -1,10 +1,10 @@
 package edu.gatech.cs2340.thericks.models;
 
+import android.util.Log;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.regex.Pattern;
-
 
 /**
  * Created by Ben Lashley on 9/19/2017.
@@ -15,20 +15,18 @@ public class UserTable {
     /** List of registered users **/
     private List<User> users = new ArrayList<>();
 
-    /** See validatePassword() for complete password criteria **/
-    private static final int MIN_PASSWORD_LENGTH = 8;
-    private static final int MAX_PASSWORD_LENGTH = 32;
-
-    private static final int MIN_USERNAME_LENGTH = 6;
-    private static final int MAX_USERNAME_LENGTH = 32;
-
     /**
      * Constructor for a new user table with pre-registered users.
      *
      * @param oldUsers List of registered users to include.
      */
     public UserTable(List<User> oldUsers) {
-        this.users.addAll(oldUsers);
+        for (User u : oldUsers) {
+            // Attempt to add user
+            if (!addUser(u)) {
+                Log.d("User Table", "User: " + u.getLogin().getUsername() + " could not be added to user table.");
+            }
+        }
     }
 
     /**
@@ -41,64 +39,34 @@ public class UserTable {
     /**
      * Creates new user with desired username and password. Then adds new user to user table.
      *
-     * @param  username Desired name for user to create
-     * @param  password Desired (unencrypted) password for
+     * @param  username Desired username
+     * @param  password Desired (unencrypted) password
      * @return true if user was created and added successfully, false if not
      */
     public boolean addUserFromData(String username, String password) {
-        // Validate username and password
-        if (!validateUsername(username) || !validatePassword(password)) {
-            return false;
-        }
-        // Create user
-        User newUser = new User(username, password);
-        if (newUser.getLogin().getSecurePassword() != null) {
-            // User was successfully created, so add to user table
-            users.add(newUser);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * Checks if a new user's desired username is valid and available to use.
-     *
-     * @param username the desired username
-     * @return true if valid and available, false if not
-     */
-    private boolean validateUsername(String username) {
-        // Check if username is valid
-        if (username == null) {
-            return false;
-        } else if (username.length() < MIN_USERNAME_LENGTH || username.length() > MAX_USERNAME_LENGTH) {
-            return false;
-        }
-        // Check if username is available
+        // Check username availability
         for (User u : users) {
             if (u.getLogin().getUsername().equals(username)) {
                 return false;
             }
         }
-        // Valid, available username
-        return true;
+        // Create new user
+        User newUser = new User(username, password);
+        return addUser(newUser);
     }
 
     /**
-     * Checks if password meets criteria to be considered valid.
-     * Criteria (must meet all):
-     *  (?=.*[0-9]) at least one digit
-     *  (?=.*[a-z]) at least one lowercase letter
-     *  (?=.*[A-Z]) at least one uppercase letter
-     *  (?=\\S+$)   no whitespace
-     *  .{MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH}     length between min and max (inclusive)
-     *
-     * @param pw the password to validate
-     * @return true if password is valid, false if not
+     * Adds existing user to user table if login is valid
+     * @param u User to be added
+     * @return true if user was added successfully, false if not
      */
-    private boolean validatePassword(String pw) {
-        final Pattern pat = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{"+ MIN_PASSWORD_LENGTH + "," + MAX_PASSWORD_LENGTH + "}$");
-        return pw != null && pat.matcher(pw).matches();
+    public boolean addUser(User u) {
+        if (u.getLogin().isValid()) {
+            users.add(u);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
