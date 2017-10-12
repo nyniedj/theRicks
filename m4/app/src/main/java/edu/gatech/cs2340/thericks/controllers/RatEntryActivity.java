80c4b46565b64a585142db1b2b217cee294b2512
Ben Lashley <blashley3@gatech.edu>
2017-10-11 23:51:50 -0400

    Added rat data details

--- m4/app/src/main/java/edu/gatech/cs2340/thericks/controllers/RatEntryActivity.java
+++ m4/app/src/main/java/edu/gatech/cs2340/thericks/controllers/RatEntryActivity.java
@@ -15,24 +15,42 @@ import edu.gatech.cs2340.thericks.models.RatData;
 public class RatEntryActivity extends AppCompatActivity {
 
     private RatData ratData;
-    private EditText dateCreated;
-    private EditText borough;
-    private EditText city;
+    private TextView key;
+    private TextView date;
+    private TextView locationType;
+    private TextView address;
+    private TextView zip;
+    private TextView borough;
+    private TextView city;
+    private TextView latitude;
+    private TextView longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rat_entry);
 
-        dateCreated = (EditText) findViewById(R.id.dateCreatedTextField);
-        borough = (EditText) findViewById(R.id.boroughTextField);
-        city = (EditText) findViewById(R.id.cityTextField);
+        key = (TextView) findViewById(R.id.rat_data_key_text);
+        date = (TextView) findViewById(R.id.rat_data_date_text);
+        locationType = (TextView) findViewById(R.id.rat_data_location_type_text);
+        address = (TextView) findViewById(R.id.rat_data_incident_address_text);
+        zip = (TextView) findViewById(R.id.rat_data_incident_zip_text);
+        borough = (TextView) findViewById(R.id.rat_data_borough_text);
+        city = (TextView) findViewById(R.id.rat_data_city_text);
+        latitude = (TextView) findViewById(R.id.rat_data_latitude_text);
+        longitude = (TextView) findViewById(R.id.rat_data_longitude_text);
 
         Bundle b = getIntent().getExtras();
         ratData = b.getParcelable("edu.gatech.cs2340.thericks.RatData");
 
-        dateCreated.setText(ratData.getCreatedDateTime());
-        borough.setText(ratData.getBorough());
-        city.setText(ratData.getCity());
+        key.setText("Key: " + ratData.getKey());
+        date.setText("Created Date and Time: " + ratData.getCreatedDateTime());
+        locationType.setText("Location type: " + ratData.getLocationType());
+        address.setText("Incident Address: " + ratData.getIncidentAddress());
+        zip.setText("Incident ZIP: " + ratData.getIncidentZip());
+        borough.setText("Borough: " + ratData.getBorough());
+        city.setText("City: " + ratData.getCity());
+        latitude.setText("Latitude: " + ratData.getLatitude());
+        longitude.setText("Longitude: " +ratData.getLongitude());
     }
 }
