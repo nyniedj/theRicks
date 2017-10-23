dc8df830a9ed57d0c13e4cf56e28be6f119eb586
CameronCacic <ccacic0@gmail.com>
2017-10-23 17:45:42 -0400

    Loads rat data and places markers on the map at the given latitude/longitude. Currently all rat data entries have 0 latitude and longitude so they all appear in the same place

--- m4/app/src/main/java/edu/gatech/cs2340/thericks/controllers/MapActivity.java
+++ m4/app/src/main/java/edu/gatech/cs2340/thericks/controllers/MapActivity.java
@@ -3,6 +3,7 @@ package edu.gatech.cs2340.thericks.controllers;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,8 +15,13 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import edu.gatech.cs2340.thericks.R;
+import edu.gatech.cs2340.thericks.database.RatDatabase;
+import edu.gatech.cs2340.thericks.database.RatTrackerApplication;
+import edu.gatech.cs2340.thericks.models.RatData;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * Created by mkcac on 10/23/2017.
@@ -23,6 +29,14 @@ import java.util.List;
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    private ArrayList<RatData> ratDataArrayList;
+    private static RatDatabase database;
+
+    /* Filters for displayed rat data */
+    private static List<Predicate<RatData>> filters;
+
+    private static ArrayAdapter<RatData> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +46,48 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        if (database == null) {
+            database = new RatDatabase(this);
+        }
+        if (filters == null) {
+            filters = new ArrayList<>();
+        }
+        if (ratDataArrayList == null) {
+            ratDataArrayList = new ArrayList<>();
+        }
+        if (adapter == null) {
+            adapter = new ArrayAdapter<RatData>(RatTrackerApplication.getAppContext(), ArrayAdapter.NO_SELECTION) {
+
+                @Override
+                public void notifyDataSetChanged() {
+                    super.notifyDataSetChanged();
+                    //mMap.clear();
+                    for (RatData r: ratDataArrayList) {
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        markerOptions.position(new LatLng(r.getLatitude(), r.getLongitude()));
+                        mMap.addMarker(markerOptions);
+                    }
+                }
+
+            };
+        }
+
+        /* NOTE: Hard coded predicates for testing display filters. Remove once user can add filters. */
+        Predicate<RatData> inNewYork = ratData -> ratData.getCity().equalsIgnoreCase("NEW YORK");
+        Predicate<RatData> commercialLocation = ratData -> ratData.getLocationType().equalsIgnoreCase("Commercial Building");
+        if (filters.isEmpty()) {
+            filters.add(inNewYork);
+            filters.add(commercialLocation);
+        }
+        /* End of predicates */
     }
 
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -47,25 +95,26 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        database.loadData(adapter, ratDataArrayList, null, filters);
+
         // Setting a click event handler for the map
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
 
             @Override
             public void onMapClick(LatLng latLng) {
 
-
-
                 // Creating a marker
-                MarkerOptions markerOptions = new MarkerOptions();
-
-                // Setting the position for the marker
-                markerOptions.position(latLng);
-
-                // Animating to the touched position
-                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-
-                // Placing a marker on the touched position
-                mMap.addMarker(markerOptions);
+//                MarkerOptions markerOptions = new MarkerOptions();
+//
+//                // Setting the position for the marker
+//                markerOptions.position(latLng);
+//
+//                // Animating to the touched position
+//                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+//
+//                // Placing a marker on the touched position
+//                mMap.addMarker(markerOptions);
             }
         });
 
@@ -93,7 +142,6 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 //
 //        @Override
 //        public View getInfoWindow(Marker marker) {
-//            // TODO Auto-generated method stub
 //            return null;
 //        }
 //
