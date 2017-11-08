3e925d9a3dfb7704c3cc2cec29a8e01f604bcd3d
CameronCacic <ccacic0@gmail.com>
2017-11-08 15:11:23 -0500

    Untested graph implementation but working somewhat

--- m4/app/src/main/java/edu/gatech/cs2340/thericks/utils/DateFilterer.java
+++ m4/app/src/main/java/edu/gatech/cs2340/thericks/utils/DateFilterer.java
@@ -3,9 +3,12 @@ package edu.gatech.cs2340.thericks.utils;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import edu.gatech.cs2340.thericks.models.RatData;
 
@@ -28,10 +31,30 @@ public class DateFilterer {
         return result;
     }
 
+    /**
+     * Returns a Predicate that filters Dates in the specified range
+     * @param begin the start date
+     * @param end the end date
+     * @return the predicate
+     */
     public static Predicate<RatData> createDateRangeFilter(Date begin, Date end) {
         return ratData -> {
             Date d = parse(ratData.getCreatedDateTime());
             return d != null && (begin == null || d.after(begin)) && (end == null || d.before(end));
         };
+    }
+
+    /**
+     * Filters the given List by the given Dats
+     * @param begin the start date
+     * @param end the end date
+     * @param data the list
+     * @return a list containing the RatData between the specified dates
+     */
+    public static List<RatData> filterByDate(Date begin, Date end, List<RatData> data) {
+        List<RatData> filteredList = new ArrayList<RatData>();
+        Predicate<RatData> predicate = createDateRangeFilter(begin, end);
+        filteredList = data.stream().filter(predicate).collect(Collectors.toList());
+        return filteredList;
     }
 }
