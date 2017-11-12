065f598ec33114e10d67891050c07fa02c10a503
Ben Lashley <blashley3@gatech.edu>
2017-11-12 16:18:45 -0500

    Array copy fix

--- m4/app/src/main/java/edu/gatech/cs2340/thericks/database/LoadRatDataTask.java
+++ m4/app/src/main/java/edu/gatech/cs2340/thericks/database/LoadRatDataTask.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
 
