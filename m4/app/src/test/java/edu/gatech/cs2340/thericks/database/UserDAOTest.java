b0db6b83983cba7bece7c2a40b18dd9ba8d8c917
DBraxton6 <dbraxton3@gatech.edu>
2017-11-12 23:32:15 -0500

    Added UserDAO JUnit test template/outline

---
+++ m4/app/src/test/java/edu/gatech/cs2340/thericks/database/UserDAOTest.java
@@ -0,0 +1,28 @@
+package edu.gatech.cs2340.thericks.database;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by DKrystine on 11/12/2017.
+ */
+public class UserDAOTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void createUser() throws Exception {
+    }
+
+    @Test
+    public void deleteUser() throws Exception {
+    }
+
+    @Test
+    public void getAllUsers() throws Exception {
+    }
+
+}
